dd4195da5071441c6d47d3069152412290849fe6
Vladislav <vladalekseev9@yandex.ru>
2020-03-10 10:42:36 +0300

    :recycle: delete comments

--- private-parking-reservation-front/src/services/AuthenticationService.js
+++ private-parking-reservation-front/src/services/AuthenticationService.js
@@ -7,65 +7,6 @@ const cookies = new Cookies();
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 export const TOKEN = 'token'
 
-class AuthenticationService {
-
-    //     executeJwtLogin(login, password) {
-    //         console.log(login);
-    //         return axios.post(`${API_URL}/login`, {
-    //             login,
-    //             password
-    //         })
-    //     }
-
-    //     executeJwtRegister(login, email, password) {
-    //         console.log(login);
-    //         return axios.post(`${API_URL}/register`, {
-    //             login,
-    //             email,
-    //             password
-    //         })
-    //     }
-
-    //     registerSuccessfulLoginForJwt(username, token) {
-    //         localStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
-    //         localStorage.setItem('AUTH', token)
-    //         cookies.set('AUTH', token, { path: '/' });
-    //         //this.setupAxiosInterceptors(/*this.createJWTToken(token)*/token)
-    //     }
-
-    //     // createJWTToken(token) {
-    //     //     return 'Bearer ' + token
-    //     // }
-
-
-    //     logout() {
-    //         localStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-    //         localStorage.removeItem(TOKEN);
-    //     }
-
-    //     isUserLoggedIn() {
-    //         let user = localStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
-    //         if (user === null) return false
-    //         return true
-    //     }
-
-    //     getLoggedInUserName() {
-    //         let user = localStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
-    //         if (user === null) return ''
-    //         return user
-    //     }
-
-    //     setupAxiosInterceptors(token) {
-    // /*        axios.interceptors.request.use(
-    //             (config) => {
-    //                 if (this.isUserLoggedIn()) {
-    //                     config.headers.AUTH = token
-    //                 }
-    //                 return config
-    //             }
-    //         )*/
-    //         axios.defaults.headers.common['AUTH'] = token;
-    //     }
-}
+class AuthenticationService {}
 
 export default new AuthenticationService()
--- private-parking-reservation-front/src/services/MessageService.js
+++ private-parking-reservation-front/src/services/MessageService.js
@@ -4,24 +4,6 @@ const url = 'http://localhost:8080'
 
 export const TOKEN = 'token'
 
-class ParkingService {
-
-    // retrieveAllMessages() {
-    //     //console.log('executed service')
-    //     return axios.get(`${url}/messages`,
-    //         //{ headers: { authorization: 'Basic ' + window.btoa(INSTRUCTOR + ":" + PASSWORD) } }
-    //         {headers: {Authorization : 'Bearer ' + localStorage.getItem(TOKEN)}});
-    // }
-
-    // connect() {
-
-    // }
-
-    // send(text) {
-    //     return axios.post(`${url}/messages`, {
-    //         text
-    //     }, {headers: {Authorization : 'Bearer ' + localStorage.getItem(TOKEN)}});
-    // }
-}
+class ParkingService {}
 
 export default new ParkingService()
