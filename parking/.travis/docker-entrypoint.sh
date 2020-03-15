411997e409b24d6d7d67482032a930861f78667d
Emil <amionv99@gmail.com>
2020-03-16 02:49:49 +0300

    :whale: Add db configuration v7
    Signed-off-by: Emil <amionv99@gmail.com>

--- parking/.travis/docker-entrypoint.sh
+++ parking/.travis/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER my_user;
     CREATE USER docker;
     CREATE DATABASE docker;
     GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
