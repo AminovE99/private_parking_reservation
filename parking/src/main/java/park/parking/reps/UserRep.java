3fc17bc51ad8231b74ac8abc3c4ebd7b8377a54f
Emil <amionv99@gmail.com>
2020-04-07 11:02:46 +0300

    :bug: Add library Optional
    Signed-off-by: Emil <amionv99@gmail.com>

--- parking/src/main/java/park/parking/reps/UserRep.java
+++ parking/src/main/java/park/parking/reps/UserRep.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import park.parking.models.User;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRep extends JpaRepository<User, Long> {
